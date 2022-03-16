de9d57a9a948dbda203d65e97111cd722fefb27f
HitoriKungZ <91017880+HitoriKung@users.noreply.github.com>
2022-03-16 12:58:03 +0700

    Create class.php

---
+++ class.php
@@ -0,0 +1,28 @@
+<?php
+class HitoriAPI{
+    public $licensekey = ''; //กรุณาใส่คีย์ คีย์จะแสดงอยู่ที่ https://n.hitorikungz.tk
+
+    public $api = 'http://49.0.195.34/gift.php';
+    //public $api = 'http://api.p5shop.in.th/gift.php';
+    function truewallet($gift){
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $this->api.'?key='.$this->licensekey.'&gift='.$gift,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array('Content-Type: application/json',),
+        ));
+        $this->response = curl_exec($curl);
+        curl_close($curl);
+        $this->data = json_decode($this->response, true);
+        return $this->data;
+    }
+
+}
+
+?>
